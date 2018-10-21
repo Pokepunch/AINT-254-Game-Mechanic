0541783a6ad261af9b29720f9a6fb0d8ef87a91f
Pokepunch <Pokepunch@users.noreply.github.com>
2018-10-21 21:14:14 +0100

    Tuned Walking and rough Dash mechanic
    This commit has some better movement and a rough but somewhat functional dash mechanic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,73 +11,144 @@ public class PlayerController : MonoBehaviour {
     private int charge;
     public int chargeMax = 70;
 
-    // Variables used to delay the start of the charge build up
-    private int chargeDelay = 0;
-    public int chargeDelayTarget = 60;
+    private int dashCooldown;
+    private int dashCooldownMax = 120;
 
-    public float velocityClamp = 30;
+    public float speed = 200;
 
-    public float maxSpeed = 15;
-    public float speed = 20;
+    private bool isOnGround;
+    private bool wasOnGround;
+
+    public float jumpForce = 900;
+    private bool isJumping = false;
+
+    public float dashForce = 1800;
+    private bool isDashing = false;
+
+    public Vector2 movement;
 
 	// Use this for initialization
 	void Start ()
     {
         body = GetComponent<Rigidbody>();
-        body.freezeRotation = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        GetJumpCharge();
-	}
+        GetJump();
+        GetDashCharge();
+    }
 
     private void FixedUpdate()
     {
-        DoMovement();
+        CheckGround();
+        GetInput();
+        DoHorizMovement();
     }
 
-    private void DoMovement()
+    public void GetInput()
     {
         float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        if (body.velocity.magnitude <= maxSpeed)
+        float y = Input.GetAxis("Vertical");
+        if (Input.GetButton("Horizontal"))
         {
-            body.AddRelativeForce(new Vector3(x * speed, 0, z *speed));
+            if (x > 0)
+            {
+                x = 1;
+            }
+            else if (x < 0)
+            {
+                x = -1;
+            }
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            if (y > 0)
+            {
+                y = 1;
+            }
+            else if (y < 0)
+            {
+                y = -1;
+            }
+        }
+        movement = new Vector2(x, y);
+    }
+
+    private void DoHorizMovement()
+    {
+        if (movement.x != 0 && !isDashing)
+        {
+            body.AddForce(movement.x * Vector2.right * speed);
         }
     }
 
-    private void GetJumpCharge()
+    private void GetDashCharge()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("ChargeDash") && dashCooldown != 0)
         {
-            if (chargeDelay < chargeDelayTarget)
+            if (charge < chargeMax)
             {
-                chargeDelay++;
+                charge += 10;
             }
-            else
+        }
+        else if (Input.GetButtonUp("ChargeDash"))
+        {
+            if (charge <= chargeMax)
             {
-                if (charge < chargeMax)
-                {
-                    charge += 10;
-                    Debug.Log("Charge = " + charge);
-                }
-                else
-                {
-                    Debug.Log("Charge is max");
-                }
+                GetInput();
+                isDashing = true;
+                body.AddForce(new Vector3(movement.x * dashForce, movement.y * dashForce, 0));
             }
+            charge = 0;
         }
         else
         {
-            chargeDelay = 0;
-        }
-        if (Input.GetButtonUp("Jump"))
-        {
-            body.AddForce(new Vector3(0, 500 + charge, 0));
             charge = 0;
+        }
+    }
+
+    private void GetJump()
+    {
+        if (Input.GetButtonDown("Jump") && !isJumping && !isDashing)
+        {
+            isJumping = true;
+            body.AddForce(new Vector3(0, jumpForce, 0));
+            charge = 0;
+        }
+    }
+
+    public void CheckGround()
+    {
+        wasOnGround = isOnGround;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f))
+        {
+            isOnGround = true;
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        if (!wasOnGround && isOnGround && isJumping)
+        {
+            isJumping = false;
+            isDashing = false;
+        }
+        if (isDashing && isOnGround)
+        {
+            isDashing = false;
+            if (dashCooldown < dashCooldownMax)
+            {
+                dashCooldown++;
+            }
+            else
+            {
+                dashCooldown = 0;
+            }
         }
     }
 }
